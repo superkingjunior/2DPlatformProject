e5cc1c1b4150e8b439ab6d3b7cd8da7a89d826b6
Collin Presser <pressercc@gmail.com>
2022-03-11 11:04:15 +0100

    Fixed Music

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -6,6 +6,14 @@ public class DontDestroy : MonoBehaviour
 {
     void Awake()
     {
+
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
         DontDestroyOnLoad(this.gameObject);
     }
 }
