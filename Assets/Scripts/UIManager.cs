885f428a2530c36fb58aa26e79123f3f0298f3db
julietdenapoli <julietdenapoli@gmail.com>
2022-03-17 11:50:40 +0100

    build setting

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -24,7 +24,7 @@ public class UIManager : MonoBehaviour
         GameObject[] objs = GameObject.FindGameObjectsWithTag("UI");
 
 
-        if (objs.Length > 1)
+        if ((objs.Length > 1)&& (objs!=null))
         {
             Destroy(instance.gameObject);
         }
